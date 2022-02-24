aab2a9aa2b4245ea55e20288c90dab7ebb1a6700
iyoussef1079 <youssef.ibrahim-moctar-diallo.1@ulaval.ca>
2022-02-24 14:29:01 -0500

    adding ordonnancer

--- Project.py
+++ Project.py
@@ -1,5 +1,8 @@
 class Project:
-    def __init__(self, nbr_days, score, best_before, nbr_roles):
-        self.details = [nbr_days, score, best_before, nbr_roles]
+    def __init__(self,name, nbr_days, Maxscore, best_before, nbr_roles):
+        self.details = [nbr_days, Maxscore, best_before, nbr_roles]
         self.skills = {}
+        self.name  = name
 
+    def addSkill(self, skillName, lvl):
+        self.skills[skillName] = lvl
