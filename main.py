6c83a5e5837ca09d5afceb4e0be0d0652bc61b38
Mjohnathan <johnathanraz@gmail.com>
2022-02-24 14:41:35 -0500

    project registration in main file

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from contributor import Contributor
-
+import Project
 files = ["a_an_example.in.txt"]
 
 
@@ -15,9 +15,15 @@ def process(files):
                 listContributor.append(contributor)
                 for j in range(int (numberSkill)):
                     skill, lvl = f.readline().split()
-            
-            
-            
+            for k in range(P):
+                name, nbr_days, score, bb, roles = f.readline().split()
+                project = Project(name, nbr_days, score, bb, roles)
+                for l in range(int(roles)):
+                    skill, lvl = f.readline().split()
+                    project.add_skill(skill, lvl)
+                listProjet.append(project)
+            for project in listProjet:
+                print(project.name)
 
         #with open("out/" + file + ".out", 'w+') as f:
         #pass # for output
