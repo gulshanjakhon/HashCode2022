362790b1eb90c503a21c54254914ccb4c9d474d6
Mjohnathan <johnathanraz@gmail.com>
2022-02-24 13:54:33 -0500

    correction on class project

--- Project.py
+++ Project.py
@@ -3,3 +3,5 @@ class Project:
         self.details = [nbr_days, score, best_before, nbr_roles]
         self.skills = {}
 
+    def add_skill(self, skill_name, lvl):
+        self.skills[skill_name] = lvl
